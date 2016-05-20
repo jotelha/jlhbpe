fc7a0df60474c2877262b52a51c7f5e27dd7058d
jotelha <johannes.laurin@gmail.com>
2016-05-20 13:11:30 +0800

    Removed minor flaws after migrating to github

--- +jlh/gitCommit.m
+++ +jlh/gitCommit.m
@@ -2,6 +2,6 @@ function gitCommit(msg)
     system('git add *.m','-echo');
     %system('git add img/*','-echo');
     %system('git add dat/*','-echo');
-    system('git add TODO','-echo');
+    %system('git add TODO','-echo');
     system(['git commit -m "',msg,'"'],'-echo');
 end
