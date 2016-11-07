642e3560ac0333611576b75c6391014fd1b55300
jotelha <johannes.laurin@gmail.com>
2016-11-07 15:57:38 +0800

    Changes on trip to shanghai

--- 2016_10_projects/bockrisSwinkel.m
+++ 2016_10_projects/bockrisSwinkel.m
@@ -167,14 +167,14 @@ end
 
 %% second figure
 hFig2 = figure(2);
-pm = 1;
-pn = 2;
+pm = 3;
+pn = 6;
 
 ax = cell(1,pm*pn);
 for i = 1:(pn*pm)
     ax{i} = subplot(pm,pn,i);
 end
-%% fit
+%% fit Langmuir, single
 N = numel(xNum);
 selection = cell(N,1);
 cDSmSel = cell(N,1);
@@ -198,17 +198,18 @@ fo.StartPoint = [0.5,5e-7];
 %fo.StartPoint = [1,1];
 fo.Lower = [0 0];
 fo.Robust = 'LAR';
-fo.Display='iter';
+%fo.Display='iter';
+fo.Display='final';
 ft = fittype('m0*beta*c/(1+beta*c)','independent','c','options',fo);
 
 %fo.Robust = 'LAR';
 langmuirFit = cell(N,1);
 for i=1:N
     langmuirFit{i} = fit(cDSmSel{i},mSel{i},ft,fo);
-    plot(ax{1},xSel{i},langmuirFit{i}(cDSmSel{i}));
-    hold(ax{1},'on');
+%     plot(ax{1},xSel{i},langmuirFit{i}(cDSmSel{i}));
+%     hold(ax{1},'on');
 end
-hold(ax{1},'off');
+% hold(ax{1},'off');
 
 % xTot = xSel{
 % for i=1:N
@@ -217,10 +218,322 @@ hold(ax{1},'off');
 %     plot(ax{1},xSel{i},langmuirFit{i}(cDSmSel{i}));
 %     hold(ax{1},'on');
 % end
-%% plot 2, adsorption reference, again
-for i = 1:N
-    plot(ax{2},xRef{i},mRef{i});
+
+%% fit Langmuir, all
+tmp = cellfun(@(c) c' ,cDSmSel,'UniformOutput',false);
+cDSmTot = [tmp{:}]';
+
+tmp = cellfun(@(c) c' ,mSel,'UniformOutput',false);
+mTot = [tmp{:}]';
+
+langmuirTotFit = fit(cDSmTot,mTot,ft,fo);
+% for i=1:N
+%     plot(ax{2},xSel{i},langmuirTotFit(cDSmSel{i}));
+%     hold(ax{2},'on');
+% end
+
+%% Langmuir plots
+hFig1 = figure(1);
+pm = 1;
+pn = 2;
+
+ax = cell(1,pm*pn);
+for i = 1:(pn*pm)
+    ax{i} = subplot(pm,pn,i);
+end
+
+
+for i=1:N
+    plot(ax{1},xSel{i}(1:20:end),mSel{i}(1:20:end),'o');
+    hold(ax{1},'on');
+    plot(ax{1},xSel{i},langmuirFit{i}(cDSmSel{i}));
+end
+hold(ax{1},'off');
+
+
+for i=1:N
+    plot(ax{2},xSel{i}(1:20:end),mSel{i}(1:20:end),'o');
     hold(ax{2},'on');
+    plot(ax{2},xSel{i},langmuirTotFit(cDSmSel{i}));
 end
 hold(ax{2},'off');
 
+%% plot 3, adsorption reference, again
+for i = 1:N
+    plot(ax{3},xRef{i},mRef{i});
+    hold(ax{3},'on');
+end
+hold(ax{3},'off');
+
+%% fit Tamkin, single
+
+% ft = fittype('m0*beta*c/(1+beta*c)','independent','c');
+% fo = fitoptions(ft);
+fo = fitoptions('Method','NonlinearLeastSquares');
+fo.TolFun=1e-12;
+fo.TolX=1e-12;
+fo.DiffMinChange=1e-12;
+fo.StartPoint = [max(mTot)/2,max(mTot)/(2*max(cDSmTot))];
+%fo.StartPoint = [1,1];
+fo.Lower = [-Inf eps];
+fo.Robust = 'LAR';
+% fo.Display='iter';
+fo.Display='final';
+ft = fittype('-A+log(c)/F','independent','c','options',fo);
+
+%fo.Robust = 'LAR';
+tamkinFit = cell(N,1);
+for i=1:N
+    tamkinFit{i} = fit(cDSmSel{i},mSel{i},ft,fo);
+    plot(ax{4},xSel{i},tamkinFit{i}(cDSmSel{i}));
+    hold(ax{4},'on');
+end
+hold(ax{4},'off');
+
+%% fit Tamkin, all
+
+tamkinTotFit = fit(cDSmTot,mTot,ft,fo);
+for i=1:N
+    plot(ax{5},xSel{i},tamkinTotFit(cDSmSel{i}));
+    hold(ax{5},'on');
+end
+
+%% fit Temkin full, single
+
+fo = fitoptions('Method','NonlinearLeastSquares');
+% fo.TolFun=1e-12;
+% fo.TolX=1e-12;
+% fo.DiffMinChange=1e-12;
+fo.StartPoint = [1/max(mTot),1,1];
+%fo.StartPoint = [1,1];
+fo.Lower = [-Inf -Inf -Inf];
+fo.Upper = [Inf Inf Inf];
+fo.Robust = 'LAR';
+fo.Display='iter';
+%fo.Display='final';
+ft = fittype('-1/f*log((1+beta1*c)/(1+beta0*c))','independent','c','options',fo);
+
+%fo.Robust = 'LAR';
+fullTemkinFit = cell(N,1);
+for i=1:N
+    fullTemkinFit{i} = fit(cDSmSel{i},mSel{i},ft,fo);
+%     plot(ax{7},xSel{i},unnamedFit{i}(cDSmSel{i}));
+%     hold(ax{7},'on');
+end
+% hold(ax{7},'off');
+
+fullTemkinTotFit = fit(cDSmTot,mTot,ft,fo);
+
+
+%% full Temkin plots
+
+hFig2 = figure(2);
+pm = 1;
+pn = 2;
+
+ax = cell(1,pm*pn);
+for i = 1:(pn*pm)
+    ax{i} = subplot(pm,pn,i);
+end
+
+
+for i=1:N
+    plot(ax{1},xSel{i}(1:20:end),mSel{i}(1:20:end),'o');
+    hold(ax{1},'on');
+    plot(ax{1},xSel{i},fullTemkinFit{i}(cDSmSel{i}));
+end
+hold(ax{1},'off');
+
+
+for i=1:N
+    plot(ax{2},xSel{i}(1:20:end),mSel{i}(1:20:end),'o');
+    hold(ax{2},'on');
+    plot(ax{2},xSel{i},fullTemkinTotFit(cDSmSel{i}));
+end
+hold(ax{2},'off');
+
+%% fit Bockris-Swinkel, single
+beta0 = exp(26280/(jlh.Constants.R*jlh.Constants.T))/55.5;
+betaInv0 = 1/beta0;
+f0 = -2.82;
+n0 = 10;
+
+% ft = fittype('betaInv*exp(-f*theta/theta0)*(theta/theta0/(1-theta/theta0)^n)*((theta/theta0+n*(1-theta/theta0))^(n-1)/n^n)','independent','theta','problem','theta0','options',fo);
+bockrisSwinkelModel = @(betaInv,f,n,theta) betaInv*exp(-f*theta).*(theta./(1-theta).^n).*((theta+n*(1-theta)).^(n-1)/n^n);
+
+%fo.Robust = 'LAR';
+bockrisSwinkelFit = cell(N,1);
+for i=1:N
+    fo = fitoptions('Method','NonlinearLeastSquares');
+    fo.StartPoint = [betaInv0 2*max(mSel{i})];
+    fo.Lower = [eps max(mSel{i})];
+    fo.Upper = [max(cDSmSel{i}) Inf];
+    fo.Robust = 'LAR';
+    fo.Display='iter';
+    %fo.Display='final';
+    ft = fittype(@(betaInv,m0,m) bockrisSwinkelModel(betaInv,f0,n0,m/m0),'independent','m');
+    bockrisSwinkelFit{i} = fit(mSel{i},cDSmSel{i},ft,fo);
+end    
+bockrisSwinkelFit2 = cell(N,1);
+for i=1:N
+    fo = fitoptions('Method','NonlinearLeastSquares');
+    % fo = fitoptions;
+    % fo.TolFun=1e-12;
+    % fo.TolX=1e-12;
+    % fo.DiffMinChange=1e-12;
+    % fo.StartPoint = [max(mTot)/2,1/(2*max(cDSmTot)),1/(2*max(cDSmTot))];
+    fo.StartPoint = [-2.82,1];
+    fo.Lower = [-10 0];
+    fo.Upper = [5 20];
+    fo.Robust = 'LAR';
+    fo.Display='iter';
+    %fo.Display='final';
+    ft = fittype(@(f,n,m) bockrisSwinkelModel(bockrisSwinkelFit{i}.betaInv,f,n,m/bockrisSwinkelFit{i}.m0),'independent','m');
+    bockrisSwinkelFit2{i} = fit(mSel{i},cDSmSel{i},ft,fo);
+end
+for i=1:N
+    fo = fitoptions('Method','NonlinearLeastSquares');
+    fo.StartPoint = [bockrisSwinkelFit{i}.betaInv bockrisSwinkelFit{i}.m0 bockrisSwinkelFit2{i}.f bockrisSwinkelFit2{i}.n];
+    fo.Lower = [eps, max(mSel{i}), -10, 0];
+    fo.Upper = [Inf Inf 5 20];
+    fo.Robust = 'LAR';
+    fo.Display='iter';
+    %fo.Display='final';
+    ft = fittype(@(betaInv,m0,f,n,m) bockrisSwinkelModel(betaInv,f,n,m/m0),'independent','m');
+    bockrisSwinkelFit3{i} = fit(mSel{i},cDSmSel{i},ft,fo);
+end 
+
+
+m0BockrisSwinkel = cell(3,1);
+for i=1:N
+    x0 = [0+eps,bockrisSwinkelFit3{i}.m0-eps];
+    for j = 1:numel(cDSmSel{i})
+        m0BockrisSwinkel{i}(j) = fzero( @(m0) bockrisSwinkelFit3{i}(m0)-cDSmSel{i}(j), x0 );
+    end 
+end 
+%% Bockris figure
+hFig2 = figure(3);
+pm = 1;
+pn = 3;
+
+ax = cell(1,pm*pn);
+for i = 1:(pn*pm)
+    ax{i} = subplot(pm,pn,i);
+end
+
+for i=1:N
+    plot(ax{1},xSel{i},bockrisSwinkelFit{i}(mSel{i}));
+    hold(ax{1},'on');
+end
+hold(ax{1},'off');
+
+
+for i=1:N
+    plot(ax{2},xSel{i}(1:20:end),mSel{i}(1:20:end),'o');
+        hold(ax{2},'on');
+    plot(ax{2},xSel{i},m0BockrisSwinkel{i});
+end
+hold(ax{2},'off');
+
+
+%% fit unknown, all
+
+bockrisSwinkelTotFit = fit(0.9*mTot/max(mTot),cDSmTot,ft,fo);
+for i=1:N
+    plot(ax{11},xSel{i},bockrisSwinkelTotFit(cDSmSel{i}));
+    hold(ax{11},'on');
+end
+hold(ax{11},'off');
+%% Flory-Huggins
+
+%fo.Robust = 'LAR';
+floryFit = cell(N,1);
+for i=1:N
+
+    fo = fitoptions('Method','NonlinearLeastSquares');
+    %fo.TolFun=min(cDSmTot)/100;
+    %fo.TolX=min(mTot);
+    %fo.DiffMinChange=min(cDSmTot)/100;
+    %fo.StartPoint = [0.5,5e-7];
+    fo.StartPoint = [max(cDSmSel{i}), max(mSel{i}), 2];
+    fo.Lower = [eps max(mSel{i})+eps 0];
+    fo.Upper = [Inf Inf 20];
+
+    %fo.Robust = 'LAR';
+    %fo.MaxIter = 100;
+    %fo.MaxFunEvals = 500;
+    fo.Display='iter';
+    %fo.Display='final';
+    ft = fittype('betaInv*m/(m0*(1-m/m0)^(n))*exp(1-n)','independent','m','options',fo);
+
+    floryFit{i} = fit(mSel{i},cDSmSel{i},ft,fo);
+    %plot(ax{4},xSel{i},floryFit{i}(mSel{i}));
+    %hold(ax{4},'on');
+end
+
+m0Flory = cell(3,1);
+for i=1:N
+    x0 = [0+eps,floryFit{i}.m0-eps];
+    for j = 1:numel(cDSmSel{i})
+        m0Flory{i}(j) = fzero( @(m0) floryFit{i}(m0)-cDSmSel{i}(j), x0 );
+    end 
+end 
+%hold(ax{4},'off');
+
+%% Fory-Huggins, all
+
+%fo.Robust = 'LAR';
+floryTotFit = fit(mTot,cDSmTot,ft,fo);
+m0FloryTot = cell(3,1);
+x0 = [0+eps,floryTotFit.m0-eps];
+for i=1:N
+    for j = 1:numel(cDSmSel{i})
+        m0FloryTot{i}(j) = fzero( @(m0) floryTotFit(m0)-cDSmSel{i}(j), x0 );
+    end 
+end 
+
+%% Flory figure
+hFig4 = figure(4);
+pm = 1;
+pn = 3;
+
+ax = cell(1,pm*pn);
+for i = 1:(pn*pm)
+    ax{i} = subplot(pm,pn,i);
+end
+
+for i=1:N
+    plot(ax{1},xSel{i},floryFit{i}(mSel{i}));
+    hold(ax{1},'on');
+end
+hold(ax{1},'off');
+
+
+for i=1:N
+    plot(ax{2},xSel{i}(1:20:end),mSel{i}(1:20:end),'o');
+    hold(ax{2},'on');
+    plot(ax{2},xSel{i},m0Flory{i});
+end
+hold(ax{2},'off');
+
+for i=1:N
+    plot(ax{3},xSel{i}(1:20:end),mSel{i}(1:20:end),'o');
+    hold(ax{3},'on');
+    plot(ax{3},xSel{i},m0FloryTot{i});
+end
+hold(ax{2},'off');
+
+
+%% plot 6, adsorption reference, again
+for i = 1:N
+    plot(ax{6},mSel{i},cDSmSel{i});
+    hold(ax{6},'on');
+end
+hold(ax{3},'off');
+
+%% plot 6, adsorption reference, again
+for i = 1:N
+    plot(ax{9},mSel{i},log(cDSmSel{i}));
+    hold(ax{9},'on');
+end
+hold(ax{3},'off');
